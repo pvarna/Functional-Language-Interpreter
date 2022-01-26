5c9caaa0d4fdbb4dad4a39d9db042faf81c11e67
pvarna <peter.kolev2001@gmail.com>
2022-01-26 03:10:40 +0200

    add dummy main

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstring>
+#include "Lexer/lexer.h"
+#include "Exceptions/illegalCharacterException.h"
+
+int main ()
+{
+    std::string str;
+
+    std::getline(std::cin, str);
+
+    Lexer lexer(str);
+
+    std::vector<Token*> tokens;
+
+    try
+    {
+        tokens = lexer.getTokens();
+    }
+    catch(std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    
+    for (Token* token : tokens)
+    {
+        std::cout << token->toString() << std::endl;
+    }
+
+    for (Token* token : tokens)
+    {
+        delete token;
+    }
+
+    tokens.clear();
+
+    return 0;
+}
