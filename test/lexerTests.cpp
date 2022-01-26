28ee664f07849ba5299879b7c03f897abdd88ad9
pvarna <peter.kolev2001@gmail.com>
2022-01-26 18:07:56 +0200

    add some more lexer tests

--- test/lexerTests.cpp
+++ test/lexerTests.cpp
@@ -132,3 +132,99 @@ TEST_CASE("Check for valid single tokens")
         deallocateTokens(tokens);
     }
 }
+
+TEST_CASE("Check some of the examples from the description")
+{
+    SECTION("Example #1")
+    {
+        Lexer ex1("isOdd -> eq(mod(int(#0), 2), 1)");
+        std::vector<Token*> tokens = ex1.getTokens();
+        REQUIRE(tokens.size() == 16);
+        REQUIRE(tokens[0]->toString() == "FUNCTION_NAME: isOdd");
+        REQUIRE(tokens[1]->toString() == "ARROW");
+        REQUIRE(tokens[2]->toString() == "FUNCTION_NAME: eq");
+        REQUIRE(tokens[3]->toString() == "OPENING_ROUND_BRACKET");
+        REQUIRE(tokens[4]->toString() == "FUNCTION_NAME: mod");
+        REQUIRE(tokens[5]->toString() == "OPENING_ROUND_BRACKET");
+        REQUIRE(tokens[6]->toString() == "FUNCTION_NAME: int");
+        REQUIRE(tokens[7]->toString() == "OPENING_ROUND_BRACKET");
+        REQUIRE(tokens[8]->toString() == "ARGUMENT: 0");
+        REQUIRE(tokens[9]->toString() == "CLOSING_ROUND_BRACKET");
+        REQUIRE(tokens[10]->toString() == "COMMA");
+        REQUIRE(tokens[11]->toString() == "WHOLE_NUMBER: 2");
+        REQUIRE(tokens[12]->toString() == "CLOSING_ROUND_BRACKET");
+        REQUIRE(tokens[13]->toString() == "COMMA");
+        REQUIRE(tokens[14]->toString() == "WHOLE_NUMBER: 1");
+        REQUIRE(tokens[15]->toString() == "CLOSING_ROUND_BRACKET");
+    }
+
+    SECTION("Example #2")
+    {
+        Lexer ex2("primes10 -> filterPrimes(concat([2], list(3, 1, 7)))");
+        std::vector<Token*> tokens = ex2.getTokens();
+        REQUIRE(tokens.size() == 20);
+        REQUIRE(tokens[0]->toString() == "FUNCTION_NAME: primes10");
+        REQUIRE(tokens[1]->toString() == "ARROW");
+        REQUIRE(tokens[2]->toString() == "FUNCTION_NAME: filterPrimes");
+        REQUIRE(tokens[3]->toString() == "OPENING_ROUND_BRACKET");
+        REQUIRE(tokens[4]->toString() == "FUNCTION_NAME: concat");
+        REQUIRE(tokens[5]->toString() == "OPENING_ROUND_BRACKET");
+        REQUIRE(tokens[6]->toString() == "OPENING_SQUARE_BRACKET");
+        REQUIRE(tokens[7]->toString() == "WHOLE_NUMBER: 2");
+        REQUIRE(tokens[8]->toString() == "CLOSING_SQUARE_BRACKET");
+        REQUIRE(tokens[9]->toString() == "COMMA");
+        REQUIRE(tokens[10]->toString() == "FUNCTION_NAME: list");
+        REQUIRE(tokens[11]->toString() == "OPENING_ROUND_BRACKET");
+        REQUIRE(tokens[12]->toString() == "WHOLE_NUMBER: 3");
+        REQUIRE(tokens[13]->toString() == "COMMA");
+        REQUIRE(tokens[14]->toString() == "WHOLE_NUMBER: 1");
+        REQUIRE(tokens[15]->toString() == "COMMA");
+        REQUIRE(tokens[16]->toString() == "WHOLE_NUMBER: 7");
+        REQUIRE(tokens[17]->toString() == "CLOSING_ROUND_BRACKET");
+        REQUIRE(tokens[18]->toString() == "CLOSING_ROUND_BRACKET");
+        REQUIRE(tokens[19]->toString() == "CLOSING_ROUND_BRACKET");
+    }
+}
+
+TEST_CASE("Some invalid inputs")
+{
+    SECTION("Invalid characters")
+    {
+        REQUIRE_THROWS(Lexer("2 + 3").getTokens());
+        REQUIRE_THROWS(Lexer("2 ? 5 : -3").getTokens());
+        REQUIRE_THROWS(Lexer("!alo").getTokens());
+    }
+
+    SECTION("Arguments")
+    {
+        REQUIRE_THROWS(Lexer("#2.3").getTokens());
+        REQUIRE_THROWS(Lexer("#-2").getTokens());
+    }
+
+    SECTION("Brackets")
+    {
+        REQUIRE_THROWS(Lexer("(").getTokens());
+        REQUIRE_THROWS(Lexer("]").getTokens());
+        REQUIRE_THROWS(Lexer("((())").getTokens());
+        REQUIRE_THROWS(Lexer("[[]]]").getTokens());
+        REQUIRE_THROWS(Lexer("(([)]]").getTokens());
+    }
+
+    SECTION("Arrow")
+    {
+        REQUIRE_THROWS(Lexer("<-").getTokens());
+        REQUIRE_THROWS(Lexer("<->").getTokens());
+    }
+
+    SECTION("Function name starting with digit")
+    {
+        REQUIRE_THROWS(Lexer("1function").getTokens());
+        REQUIRE_THROWS(Lexer("23alo").getTokens());
+    }
+
+    SECTION("Numbers")
+    {
+        REQUIRE_THROWS(Lexer("23.00.0").getTokens());
+        REQUIRE_THROWS(Lexer("16f.05").getTokens());
+    }
+}
